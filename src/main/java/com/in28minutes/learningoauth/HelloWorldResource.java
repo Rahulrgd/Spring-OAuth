0f6032f08872fea195e8832e231e24e6c71f1287
RahulGupta <rahulrgd21@gmail.com>
2024-01-24 18:42:36 +0530

    Step 24 - Getting Started with Spring Boot and OAuth2 - Login with Google

---
+++ src/main/java/com/in28minutes/learningoauth/HelloWorldResource.java
@@ -0,0 +1,16 @@
+package com.in28minutes.learningoauth;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldResource {
+    
+    @GetMapping("/hello-world")
+    public String helloWorld(Authentication authentication){
+        System.out.println(authentication);
+        System.out.println(authentication.getPrincipal());
+        return "Hello World";
+    }
+}
